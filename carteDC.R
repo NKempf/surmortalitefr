a81a12dc627b5344bd320930dfe1b7b96cd9f649
NKempf <kempfnicolas@gmail.com>
2021-02-06 16:18:56 +0100

    Maj prog et ajout des données

--- carteDC.R
+++ carteDC.R
@@ -1,128 +1,102 @@
 #-------------------------------------------------------------------------------------------------#
-#                       Carte intéractive des DC par départements                                 #
+#                 Interactive map of excess mortality in France during Covid crisis               #
 #-------------------------------------------------------------------------------------------------#
 
-# Packages nécessaires
-# library(shiny)
-# library(leaflet)
-library(sf)
-library(tidyverse)
-library(lubridate)
-library(zoo)
-library(RColorBrewer)
+# Packages
+library(sf) # spatial object
+library(tidyverse) # tidy data
+library(lubridate) # date format
+library(zoo) # Moving average
+library(shiny) # App
+library(leaflet) # Interactive map
+library(RColorBrewer) # Color palette
 library(htmltools)
 
-# I. Préparation des données
-#--------------------------------------------------------------------------------------
+# I. Import and tidy data
+#------------------------------------------------------------------------------------------------
 
-# departement_filtre <- c("85","49","37","36","18","03","63","15","46","82","32","65",
-#                         "64","40","47","33","24","19","87","23","16","17","79","86")
+# Region map ("https://www.data.gouv.fr/fr/datasets/r/aacf9338-8944-4513-a7b9-4cd7c2db2fa9")
+reg <- st_read("maps/region/regions-20180101.shp",options = "ENCODING=UTF-8") %>% 
+  st_simplify(preserveTopology = T,0.008) %>%  # simplify geometry
+  st_transform(4326) %>% # new coordinate system
+  mutate(label = paste0(nom,"(",code_insee,")")) %>%
+  select(code_insee,label)
 
-# I.1. Cartes
-reg <- st_read("data/maps/reg_francemetro_2019.shp",options = "ENCODING=WINDOWS-1252") %>% 
-  st_transform(4326) %>% 
-  mutate(label = paste0(libelle,"(",code,")")) %>% 
-  select(label)
-dep <- st_read("data/maps/dep_francemetro_2019.shp",options = "ENCODING=WINDOWS-1252") %>% 
-  st_transform(4326) %>% 
-  mutate(label = paste0(libelle,"(",code,")")) 
-# com <- st_read("data/maps/commune_francemetro_2019.shp",options = "ENCODING=WINDOWS-1252") %>%
-#   filter(dep %in% departement_filtre) %>% 
-#   st_simplify(dTolerance = 100) %>% 
-#   st_transform(4326) %>% 
-#   mutate(label = paste0(libelle,"(",code,")")) %>% 
-#   select(label)
+# Departments map (https://www.data.gouv.fr/fr/datasets/contours-des-departements-francais-issus-d-openstreetmap/)
+dep <- st_read("maps/departments/departements-20180101.shp",options = "ENCODING=UTF-8") %>% 
+  st_simplify(preserveTopology = T,0.008) %>%  # simplify geometry
+  st_transform(4326) %>% # new coordinate system
+  mutate(label = paste0(nom,"(",code_insee,")")) 
 
+# death daily data (https://www.insee.fr/fr/statistiques/4487988)
+death <- bind_rows(
+  read.csv2("data/death/DC_2018_det.csv",stringsAsFactors = FALSE),
+  read.csv2("data/death/DC_2019_det.csv",stringsAsFactors = FALSE),
+  read.csv2("data/death/DC_20202021_det.csv",stringsAsFactors = FALSE)
+) %>%
+  mutate(date_dc = as.Date(parse_date_time(paste0(JDEC,"-",MDEC,"-",ADEC),orders = "dmy")))
 
-# I.2. Borne de découpe des couleurs
+# II. Statistical computation
+#------------------------------------------------------------------------------------------------
+death_dep <- death %>% 
+  mutate(week_iso = isoweek(date_dc)) %>% 
+  group_by(DEPDEC, ADEC, week_iso) %>% 
+  summarise(total_dc = n()) %>% # weekly death by departments
+  ungroup() %>% 
+  group_by(DEPDEC) %>% 
+  mutate(roll_mean = rollmean(total_dc, 3, na.pad = T),
+         roll_mean = ifelse(is.na(roll_mean),total_dc,roll_mean)) %>% # Moving average => smoothing data
+  select(-total_dc) %>% 
+  filter(!is.na(roll_mean)) %>%
+  spread(key = ADEC,value = roll_mean) %>% 
+  mutate(death_ref = (`2018` + `2019`) / 2 ) %>% # Weekly average death (2018-2019) 
+  select(-`2018`,-`2019`) %>% 
+  gather(key = an, value = death, -DEPDEC,-week_iso,-death_ref) %>% 
+  filter(!is.na(death_ref) & !is.na(death)) %>%   
+  mutate(surmortalite = 100* death / death_ref, # Excess mortality
+         week_first_day = floor_date(ymd(paste0(an,"-01-01")) + weeks(week_iso), "weeks",week_start = 1),
+         week_first_day.f = factor(format(week_first_day,"%d %b %y"),levels = unique(format(week_first_day,"%d %b %y")))
+  ) %>% 
+  ungroup() 
+ 
+# II. Application Shiny
+#-----------------------------------------------------------------------------------------------------------------
+
+# Color
 couleur_surmortalite <- c(-Inf, 80,90,98,102,110,120,Inf)
 palette_couleur_surmortalite <- colorBin("RdYlGn", domain = c(0,200), bins = couleur_surmortalite,reverse = TRUE)
 
-# I.3. Décés 
-
-departements <- read.csv2("data/metadata/departement.csv",sep = ",",stringsAsFactors = F,header = F) %>% 
-  rename(codegeo = V2, libelle = V5)
-
-dc <- bind_rows(
-  read.csv2("data/D14/DC_2018_det.csv",stringsAsFactors = FALSE),
-  read.csv2("data/D14/DC_2019_det.csv",stringsAsFactors = FALSE),
-  read.csv2("data/D14/DC_2020_det.csv",stringsAsFactors = FALSE)
-) %>% 
-  mutate(date_dc = as.Date(parse_date_time(paste0(JDEC,"-",MDEC,"-",ADEC),orders = "dmy")))
-
-# DC departements Nouvelle-Aquitaine
-dc_dep <- dc %>% 
-  # filter(DEPDEC %in% departements_NA) %>% 
-  # slice(1:10000) %>%
-  group_by(DEPDEC,ADEC,date_dc) %>%
-  summarise(total_dc = n()) %>%
-  left_join(departements %>% 
-              select(codegeo,geo=libelle), by = c("DEPDEC" = "codegeo")) %>% 
-  rename(codegeo=DEPDEC)
-
-# Décés journalier par geographie
-dc2 <- dc_dep %>% 
-  mutate(semaine_iso = isoweek(date_dc)) %>% 
-  group_by(codegeo,geo, ADEC, semaine_iso) %>% 
-  summarise(total_dc = sum(total_dc)) %>% 
-  ungroup() %>% 
-  # mutate(Date = floor_date(ymd(paste0(ADEC,"-01-01")) + weeks(semaine_iso), "weeks",week_start = 1)) %>% 
-  group_by(ADEC,codegeo,geo) %>% 
-  mutate(roll_mean = rollmean(total_dc, 3, na.pad = T),
-         # date_dc2 = format(date_dc, format="%d-%m")
-  ) %>% 
-  select(-total_dc) %>% 
-  filter(!is.na(roll_mean)) %>% 
-  spread(key = ADEC,value = roll_mean) %>% 
-  mutate(surmortalite = 100*2* `2020` / (`2018` + `2019`),
-         Date = floor_date(ymd("2020-01-01") + weeks(semaine_iso), "weeks",week_start = 1),
-         date.f = factor(format(Date,"%d-%b"),levels = unique(format(Date,"%d-%b")))
-         ) %>% 
-  ungroup() %>% 
-  mutate(dc20 = round(`2020`),
-         dc1819_moyen = round((`2019` + `2018`)/2))
-
-# II. Application Shiny
-#-----------------------------------------------------------------------------------------------------------------
+#*********************************************************************************************
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("mapAct", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
-                # sliderInput("animation", "Semaine",
-                #             min = as.POSIXct("2017-02-15 00:00:00",tz = "Europe/Budapest"),
-                #             max = as.POSIXct("2017-02-15 23:59:59",tz = "Europe/Budapest"),
-                #             value = as.POSIXct("2017-02-15 00:00:00",tz = "Europe/Budapest"),
-                #             timezone = "+0200",
-                #             animate =
-                #               animationOptions(interval = 600, loop = TRUE))
-                
-                sliderInput("animation", "Semaines",
-                            min = 3, max = 43,
-                            value = 1, step = 1,
+                sliderInput("animation", "Date",
+                            min = min(death_dep$week_first_day), 
+                            max = max(death_dep$week_first_day),
+                            value = death_dep$week_first_day[1], 
+                            timeFormat="%d %b %y",
+                            step = 7,
                             animate =
-                              animationOptions(interval = 500, loop = FALSE))
-                
+                              animationOptions(interval = 1000, loop = FALSE))
   )
-  
 )
 
-# Define server logic required
+#*********************************************************************************************
 server <- function(input, output) {
   # Données département filtrées à partir du slider input
   filteredData <- reactive({
-    
-    print(input$animation)
-    
-    dep %>% 
-      left_join(dc2 %>% filter(semaine_iso == input$animation) %>% 
-                  ungroup() %>% 
-                  select(codegeo,surmortalite,date.f,dc20,dc1819_moyen), by=c("code" = "codegeo") ) %>% 
-      mutate(label_surmortalite = paste0("Département : ",label,"<br/>Date : ",date.f ,
-                                         "<br/>Surmortalite : ", round(surmortalite,1),
-                                         "<br/>Nb décès 2020 : ",dc20,
-                                         "<br/>Moyenne décès 2018-2019 : ",dc1819_moyen))
+
+    dep %>%
+      left_join(death_dep %>% 
+                  filter(week_first_day == input$animation) %>%
+                  ungroup() %>%
+                  select(codegeo=DEPDEC,surmortalite,week_first_day.f,death,death_ref), by=c("code_insee" = "codegeo") ) %>%
+      mutate(label_surmortalite = paste0("Area : ",label,"<br/>Date : ",week_first_day.f ,
+                                         "<br/>Excess mortality (base 100) : ", round(surmortalite,1),
+                                         "<br/>Smoot number weekly death : ",round(death,0),
+                                         "<br/>Average number of death 2018-2019 : ",round(death_ref,0)))
   })
-  
   
   output$mapAct<-renderLeaflet({
     leaflet(options = leafletOptions(zoomControl = TRUE,
@@ -130,7 +104,7 @@ server <- function(input, output) {
       addTiles(group = "OSM (default)") %>%
       addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>%
       # Région
-      addPolygons(data=reg,opacity = 3,group = "Région",
+      addPolygons(data=reg,opacity = 3,group = "Region",
                   color = "#2F9599", stroke = TRUE, weight = 2,
                   fill = TRUE, fillOpacity = 0.01,
                   label = ~paste(label),
@@ -138,7 +112,7 @@ server <- function(input, output) {
                     color = "#F26B38", opacity = 1, weight = 3, fillOpacity = 0,
                     bringToFront = FALSE, sendToBack = TRUE)) %>%
       # Départements
-      addPolygons(data=dep,opacity = 4,group = "Départements",
+      addPolygons(data=dep,opacity = 4,group = "Departments",
                   color = "#CC527A", stroke = TRUE, weight = 2,
                   fill = TRUE, fillOpacity = 0.01,
                   label = ~paste(label),
@@ -147,17 +121,17 @@ server <- function(input, output) {
                     bringToFront = TRUE, sendToBack = TRUE)) %>% 
       addLegend(pal = palette_couleur_surmortalite, values = c(0,200),
                   # dc2$surmortalite[dc2$semaine_iso == 2], 
-                opacity = 0.7, title = "Surmortalité (base 100)",
+                opacity = 0.7, title = "Excess mortality (base 100)",
                 position = "bottomright") %>% 
       addLayersControl(
         baseGroups = c("OSM (default)", "Satellite"),
-        overlayGroups = c("Région","Départements","Surmortalite"
+        overlayGroups = c("Region","Departments","Excess mortality"
                           
         ),
         options = layersControlOptions(collapsed = FALSE),
         position = "bottomleft"
       ) %>% 
-      hideGroup(c("Région","Départements"))
+      hideGroup(c("Region","Departments"))
       
       # fitBounds(lng1 = 5,lat1 = 52,lng2 = 5.2,lat2 = 52.2)# set to reactive minimums
   })
@@ -165,7 +139,7 @@ server <- function(input, output) {
   observe({
     leafletProxy("mapAct") %>%
       # clearShapes() %>%
-      addPolygons(data = filteredData(),layerId = filteredData()$code,
+      addPolygons(data = filteredData(),layerId = filteredData()$code_insee,
                   fillColor = ~palette_couleur_surmortalite(filteredData()$surmortalite),
                   weight = 2,
                   opacity = 1,
@@ -179,7 +153,7 @@ server <- function(input, output) {
                     dashArray = "",
                     fillOpacity = 0.7,
                     bringToFront = TRUE)
-      ) 
+      )
   })
   
 }
